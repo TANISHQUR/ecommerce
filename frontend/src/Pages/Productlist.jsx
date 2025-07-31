c3a36528f5e3fc7bba5626d77acdc65b486ae8a4
TANISHQUR <kumartanishq2005@gmail.com>
2025-07-31 21:46:07 +0530

    commit

--- frontend/src/Pages/Productlist.jsx
+++ frontend/src/Pages/Productlist.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
-import './ProductList.css';
+import './Productlist.css';
 
 const ProductList = () => {
   const [products, setProducts] = useState([]);
